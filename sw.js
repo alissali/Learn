bc1269b18a49fcfe5e5fb61bdc77a0db7d4c5459
Alissali Mamoun <67880016+alissali@users.noreply.github.com>
2026-06-12 17:09:34 +0200

    🔗 Canonical URLs (13 pages) + 📴 Service Worker for ARCADE PWA

--- sw.js
+++ sw.js
@@ -1,18 +1,35 @@
-const CACHE = 'jux-arcade-v1';
-const URLS = [
-  'ARCADE.html',
-  'SNAKE.html',
-  'JuXYams.html',
-  'MONOPOLY.html'
+const CACHE_NAME = 'jux-arcade-v1';
+const ASSETS = [
+  '/Learn/ARCADE.html',
+  '/Learn/SNAKE.html',
+  '/Learn/JuXYams.html',
+  '/Learn/HOLDEM.TRAINING.html',
+  '/Learn/DUNE.TAROT.PORN.html',
+  '/Learn/DUNE.TAROT.GAME.html',
+  '/Learn/DUNE.TAROT.html',
+  '/Learn/MONOPOLY.html',
+  '/Learn/SSS.html',
+  '/Learn/CAVERN.html'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
